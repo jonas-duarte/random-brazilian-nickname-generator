92a8f51368ee80997e38b27af1224ce51f510ded
Santos <jonas.santos@sap.com>
2022-02-02 12:38:54 -0300

    changed random algo

--- api/index.js
+++ api/index.js
@@ -3,11 +3,25 @@ const app = require('express')();
 const Rbng = require("../domain/rbng")
 
 function randomGenerate() {
-    let rbng = new Rbng(new Date().getTime())
-    if (Rbng.getRandomInteger() % 5 === 0) rbng = rbng.mapToRandomNumberSubstitution()
-    if (Rbng.getRandomInteger() % 5 === 0) rbng = rbng.mapToRandomUpperCase()
-    if (Rbng.getRandomInteger() % 3 === 0) rbng = rbng.mapToRandomNumberAtEnding()
-    return rbng
+  let rbng = new Rbng(new Date().getTime())
+
+
+  let randomFunctions = Rbng.getRandomInteger() % 5 - 1
+
+  if (randomFunctions < 1) randomFunctions = 1
+
+  let randomFunctionsPool = [
+    "mapToRandomNumberSubstitution",
+    "mapToRandomUpperCase",
+    "mapToRandomNumberAtEnding",
+  ]
+
+  while (randomFunctions > 0) {
+    rbng = rbng[randomFunctionsPool[Rbng.getRandomInteger() % randomFunctionsPool.length]]()
+    randomFunctions--
+  }
+
+  return rbng
 }
 
 app.get('/api', (req, res) => {
